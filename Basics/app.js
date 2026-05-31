6f72236b42b90d642cdec3571ecb220056f8d3f8
Ronnel <logdatronnell@gmail.com>
2026-05-31 17:41:56 +0800

    initial commit

---
+++ Basics/app.js
@@ -0,0 +1,52 @@
+// STRING CONCATENATION
+// let name = "Ronnel";
+// let what_do_you_wanna_become_in_life = "Programmer";
+// let gender = "male";
+// let fb_name = "Ronnel M. Dela Cruz";
+
+// console.log(
+//   "I am " +
+//     name +
+//     " and I want to become a " +
+//     what_do_you_wanna_become_in_life +
+//     " in the future. I am a " +
+//     gender +
+//     " and my facebook name is " +
+//     fb_name +
+//     ".",
+// );
+
+// ARITHMETIC OPERATORS
+// let fnum = 10;
+// let snum = 20;
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function divide(a, b) {
+//   return a / b;
+// }
+
+// function modulus(a, b) {
+//   return a % b;
+// }
+
+// function exponent(a, b) {
+//   return a ** b;
+// }
+
+// console.log(add(fnum, snum));
+// console.log(subtract(fnum, snum));
+// console.log(multiply(fnum, snum));
+// console.log(divide(fnum, snum));
+// console.log(modulus(fnum, snum));
+// console.log(exponent(fnum, snum));
