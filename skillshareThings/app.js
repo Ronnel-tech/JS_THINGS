eb0c9ba9342ff94ec54293c05f21ed32df94107d
Ronnel <logdatronnell@gmail.com>
2026-09-19 20:13:39 +0800

    learned about reduce() method

--- skillshareThings/app.js
+++ skillshareThings/app.js
@@ -1,45 +1,102 @@
 "use strict";
 
-const songs = [];
+// const songs = [];
+//
+// const song1 = {
+//   song: "AShape of you1",
+//   timeStreamed: 1.384,
+//   wonGrammy: true,
+// };
+// const song2 = {
+//   song: "AShape of you2",
+//   timeStreamed: 2.384,
+//   wonGrammy: true,
+// };
+// const song3 = {
+//   song: "Shape of you3",
+//   timeStreamed: 2.384,
+//   wonGrammy: true,
+// };
+// const song4 = {
+//   song: "Shape of you4",
+//   timeStreamed: 2.384,
+//   wonGrammy: false,
+// };
+//
+// songs.push(song1, song2, song3, song4);
+//
+// const hasWonGrammy = songs.some((song) => song.wonGrammy === true);
+// const hasTimeStreamed = songs.every((song) => song.timeStreamed > 1.5);
+//
+// console.log(hasWonGrammy, hasTimeStreamed);
+//
+// songs
+//   .map((song) =>
+//     song.timeStreamed > 2.3
+//       ? { ...song, great: true }
+//       : { ...song, great: false },
+//   )
+//   .forEach((song) => {
+//     if (song.great) {
+//       console.log(`The ${song.song} is great`);
+//     }
+//   });
+//
+// const res = songs.filter((song) => song.timeStreamed > 2.3);
+// console.log(res);
 
-const song1 = {
-  song: "Shape of you1",
-  timeStreamed: 1.384,
-  wonGrammy: true,
-};
-const song2 = {
-  song: "Shape of you2",
-  timeStreamed: 2.384,
-  wonGrammy: true,
-};
-const song3 = {
-  song: "Shape of you3",
-  timeStreamed: 2.384,
-  wonGrammy: true,
-};
-const song4 = {
-  song: "Shape of you4",
-  timeStreamed: 2.384,
-  wonGrammy: false,
-};
+// const cars = [];
+//
+// const car1 = {
+//   name: "Toyota",
+//   isElectric: false,
+//   year: 2023,
+//   color: "red",
+// };
+// const car2 = {
+//   name: "Ford",
+//   isElectric: false,
+//   year: 2022,
+//   color: "red",
+// };
+// const car3 = {
+//   name: "Volkswagen",
+//   isElectric: true,
+//   year: 2003,
+//   color: "white",
+// };
+// const car4 = {
+//   name: "Honda",
+//   isElectric: false,
+//   year: 2021,
+//   color: "yellow",
+// };
+// const car5 = {
+//   name: "BMW",
+//   isElectric: true,
+//   year: 2000,
+//   color: "red",
+// };
+// cars.push(car1, car2, car3, car4, car5);
+//
+// const totalYear = cars.reduce((sum, car) => {
+//   if (!car.isElectric) {
+//     return sum + car.year;
+//   } else {
+//     return sum;
+//   }
+// }, 0);
+//
+// console.log(totalYear);
 
-songs.push(song1, song2, song3, song4);
+const numbers = [];
+numbers.push(1, 2, 3, 4, 5);
 
-const hasWonGrammy = songs.some((song) => song.wonGrammy === true);
-const hasTimeStreamed = songs.every((song) => song.timeStreamed > 1.5);
+const multiplyNumber = numbers.reduce((reducer, number) => {
+  if (number > 3) {
+    reducer.push(number);
+  }
+  return reducer;
+}, []);
 
-console.log(hasWonGrammy, hasTimeStreamed);
-
-songs
-  .map((song) =>
-    song.timeStreamed > 2.3
-      ? { ...song, great: true }
-      : { ...song, great: false },
-  )
-  .forEach((song) => {
-    if (song.great) {
-      console.log(`The ${song.song} is great`);
-    }
-  });
-
-console.log(songs);
+console.log(multiplyNumber);
